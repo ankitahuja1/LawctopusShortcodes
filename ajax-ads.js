5870cd5c2b0bb65654c721108c2c02993a4d0109
ankitahuja1 <adxdxa@gmail.com>
2022-04-16 05:54:36 +0530

    working with params in ads

--- ajax-ads.js
+++ ajax-ads.js
@@ -1,9 +1,3 @@
-// Select the node that will be observed for mutations
-const targetNode = document.getElementById('home-posts-ad');
-
-// Options for the observer (which mutations to observe)
-const config = { attributes: false, childList: true, subtree: true };
-
 //prototype to add ads
 Element.prototype.appendBeforeThis = function (element) {
     this.parentNode.insertBefore(element, this);
@@ -18,27 +12,22 @@ NewElement.innerHTML = 'New Element';
 NewElement.className = 'code-block-4';
 
 // Callback function to execute when mutations are observed
-var adsInsert = () => {
+var adsInsert = postsAfter => {
     let postListAds = document.querySelectorAll('.jet-listing-grid__item');
     let adsCount = document.querySelectorAll('.code-block-4');
-    console.log('A child node has been added or removed.');
-    for (i=1; i<postListAds.length/3; i++){
+    for (i=1; i<postListAds.length/postsAfter; i++){
         console.log(postListAds[i].previousElementSibling);
-        if(!postListAds[i*3].previousElementSibling.classList.contains('code-block-4')){
-          NewElement.cloneNode(true).appendBefore(postListAds[i*3]);
+        if(!postListAds[i*postsAfter].previousElementSibling.classList.contains('code-block-4')){
+          NewElement.cloneNode(true).appendBefore(postListAds[i*postsAfter]);
         }
     }
 } 
 
-(() => { adsInsert(); setInterval(adsInsert, 1000); })();
+adsInsert(3);
 
-//document.querySelector('#btn-load-more').addEventListener('click', callback);
+const callback = () => { setTimeout(adsInsert , 1000, 3); setTimeout(adsInsert, 2000, 3); setTimeout(adsInsert, 3000, 3); setTimeout(adsInsert, 4000, 3); setTimeout(adsInsert, 5000, 3); };
 
-// Create an observer instance linked to the callback function
-//const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-//observer.observe(targetNode, config)
+document.querySelector('#btn-load-more').addEventListener('click', callback);
 
 var ads = 
 [
